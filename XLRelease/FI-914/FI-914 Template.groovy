a82c0ee0319459f96ab766210fb8876ce87338a3
Release Administrator <>
2021-11-16 06:47:36 -0500

    v2

---
+++ XLRelease/FI-914/FI-914 Template.groovy
@@ -0,0 +1,21 @@
+// Exported from:        http://LAPTOP-9C20LTLG:5516/#/templates/Folderc7e178190a1840b78f18aab0edb6b2de-Releasedd211bef3c744ab1919f8efcf4cadafb/code
+// Release version:      10.2.0-20210603-123974
+// Date created:         Tue Nov 16 06:47:36 EST 2021
+
+xlr {
+  template('FI-914 Template') {
+    folder('FI-914')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2021-11-15T22:30:00-0500')
+    phases {
+      phase('Dummy Phase') {
+        color '#3D6C9E'
+        tasks {
+          manual('Manual Task') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
