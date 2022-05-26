6443d790da05d9266cf2b86af265883278f548ab
Release Administrator <sivaAdmin213@gmail.com>
2022-05-26 13:24:44 +0530

    FFG1

---
+++ XLRelease/D-21279/FFQ.groovy
@@ -0,0 +1,23 @@
+// Exported from:        http://LAPTOP-9C20LTLG:5516/#/templates/Folderaaeae0c3ecbe4b3abb7918631de1fe82-Release63aa428539c14836a1253ea69906cd23/code
+// Release version:      22.1.3
+// Date created:         Thu May 26 13:24:43 IST 2022
+
+xlr {
+  template('FFQ') {
+    folder('D-21279')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2022-05-26T09:00:00+0530')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('DD') {
+            
+          }
+          manual('FFFG') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
