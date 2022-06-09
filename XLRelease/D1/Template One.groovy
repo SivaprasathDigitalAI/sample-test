077e59b56ec80cf397cb294b9803d5c1e8096050
SivaprasathDigitalAI <85228170+SivaprasathDigitalAI@users.noreply.github.com>
2022-06-09 19:51:16 +0530

    V6

--- XLRelease/D1/Template One.groovy
+++ XLRelease/D1/Template One.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://LAPTOP-9C20LTLG:5516/#/templates/Folder61f164c9347544038cc8466685b62ce9-Release55f475723d354a52b60d5f7b261c9801/code
 // Release version:      22.0.0-20211220-123974
-// Date created:         Thu Jun 09 19:32:14 IST 2022
+// Date created:         Thu Jun 09 19:51:14 IST 2022
 
 xlr {
   template('Template One') {
@@ -18,6 +18,9 @@ xlr {
       phase('New Phase') {
         tasks {
           manual('D') {
+            
+          }
+          manual('DD') {
             
           }
         }
