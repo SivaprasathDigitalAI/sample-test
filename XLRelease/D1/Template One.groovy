f9800526edd166a8c29031f17e232ac65b4a72ef
SivaprasathDigitalAI <85228170+SivaprasathDigitalAI@users.noreply.github.com>
2022-06-10 12:37:44 +0530

    V9

--- XLRelease/D1/Template One.groovy
+++ XLRelease/D1/Template One.groovy
@@ -1,20 +1,16 @@
-// Exported from:        http://LAPTOP-9C20LTLG:5516/#/templates/Folder61f164c9347544038cc8466685b62ce9-Release55f475723d354a52b60d5f7b261c9801/code
+// Exported from:        http://LAPTOP-9C20LTLG:5516/#/templates/Folder61f164c9347544038cc8466685b62ce9-Release1c88784eef3f458f8fe16992c461f84d/code
 // Release version:      22.0.0-20211220-123974
-// Date created:         Thu Jun 09 19:53:53 IST 2022
+// Date created:         Fri Jun 10 12:37:41 IST 2022
 
 xlr {
   template('Template One') {
     folder('D1')
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2022-06-09T09:00:00+0530')
     scriptUsername 'admin'
-    scriptUserPassword '{aes:v0}0N4goykgAdBzGWXpjIip4Pdc3oIiMWA6+MtmJ7d4+k0='
     phases {
       phase('New Phase') {
         tasks {
-          manual('D') {
-            
-          }
-          manual('DD') {
+          manual('MT1') {
             
           }
         }
